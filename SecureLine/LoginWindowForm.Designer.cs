da73b2bc44cdbe6a9f29528e03eeff512dbbe3aa
MansatCode <bhattacharyya.samuel@gmail.com>
2021-01-01 20:42:36 +0000

    First iteration of LogInForm created.

---
+++ SecureLine/LoginWindowForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SecureLine
+{
+    partial class LoginWindowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginWindowForm));
+            this.LabelHeader = new System.Windows.Forms.Label();
+            this.TextBoxUsername = new System.Windows.Forms.TextBox();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.LabelLogInFailed = new System.Windows.Forms.Label();
+            this.ButtonLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabelHeader
+            // 
+            this.LabelHeader.AutoSize = true;
+            this.LabelHeader.Font = new System.Drawing.Font("Segoe UI Semilight", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelHeader.Location = new System.Drawing.Point(35, 29);
+            this.LabelHeader.Name = "LabelHeader";
+            this.LabelHeader.Size = new System.Drawing.Size(203, 50);
+            this.LabelHeader.TabIndex = 0;
+            this.LabelHeader.Text = "Secure Line";
+            this.LabelHeader.Click += new System.EventHandler(this.headerLabel_Click);
+            // 
+            // TextBoxUsername
+            // 
+            this.TextBoxUsername.Location = new System.Drawing.Point(43, 103);
+            this.TextBoxUsername.Name = "TextBoxUsername";
+            this.TextBoxUsername.Size = new System.Drawing.Size(337, 35);
+            this.TextBoxUsername.TabIndex = 2;
+            this.TextBoxUsername.Text = "ID";
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Location = new System.Drawing.Point(43, 144);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.Size = new System.Drawing.Size(337, 35);
+            this.TextBoxPassword.TabIndex = 3;
+            this.TextBoxPassword.Text = "Password";
+            // 
+            // LabelLogInFailed
+            // 
+            this.LabelLogInFailed.AutoSize = true;
+            this.LabelLogInFailed.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelLogInFailed.Location = new System.Drawing.Point(37, 238);
+            this.LabelLogInFailed.Name = "LabelLogInFailed";
+            this.LabelLogInFailed.Size = new System.Drawing.Size(449, 37);
+            this.LabelLogInFailed.TabIndex = 4;
+            this.LabelLogInFailed.Text = "Log in unsuccessful. Please try again.";
+            // 
+            // ButtonLogIn
+            // 
+            this.ButtonLogIn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonLogIn.Location = new System.Drawing.Point(43, 185);
+            this.ButtonLogIn.Name = "ButtonLogIn";
+            this.ButtonLogIn.Size = new System.Drawing.Size(104, 32);
+            this.ButtonLogIn.TabIndex = 5;
+            this.ButtonLogIn.Text = "Log In";
+            this.ButtonLogIn.UseVisualStyleBackColor = true;
+            // 
+            // LoginWindowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 314);
+            this.Controls.Add(this.ButtonLogIn);
+            this.Controls.Add(this.LabelLogInFailed);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.TextBoxUsername);
+            this.Controls.Add(this.LabelHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Name = "LoginWindowForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secure Line";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelHeader;
+        private System.Windows.Forms.TextBox TextBoxUsername;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.Label LabelLogInFailed;
+        private System.Windows.Forms.Button ButtonLogIn;
+    }
+}
+
--- SecureLine/LoginWindowForm.cs
+++ SecureLine/LoginWindowForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace SecureLine
 {
-    public partial class Form1 : Form
+    public partial class LoginWindowForm : Form
     {
-        public Form1()
+        public LoginWindowForm()
         {
             InitializeComponent();
+        }
+
+        private void headerLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
